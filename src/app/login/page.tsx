d7142f44df092fbe891e53bdcac6452cd94deb81
sijun <sionwer5@gmail.com>
2025-12-14 11:23:32 +0900

    feat: login ui 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { loginWithGoogle } from "@/supabase/auth";
+import Image from "next/image";
 
 export default function LoginPage() {
   const handleGoogleLogin = async () => {
@@ -9,14 +10,15 @@ export default function LoginPage() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-50">
-      <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow-lg">
+      <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow-sm">
         <h1 className="text-2xl font-semibold mb-6 text-center">로그인</h1>
 
         <button
           onClick={handleGoogleLogin}
-          className="mt-4 w-full rounded-md border border-slate-200 py-2 text-sm font-medium hover:bg-slate-50 cursor-pointer"
+          className="mt-4 flex w-full items-center justify-center gap-2 rounded-md border border-slate-200 py-2 text-sm font-medium hover:bg-slate-50 cursor-pointer"
         >
-          Google로 계속하기
+          <Image src="/google.svg" alt="Google" width={16} height={16} />
+          Google login
         </button>
       </div>
     </div>
